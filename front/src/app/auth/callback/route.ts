7195d32be088aefca1722df8a64d8888c4ee515a
stevennkeneng <nkeneng.junior@yahoo.fr>
2025-02-05 15:03:04 +0100

    changed redirect url to be used from .env for prod

--- front/src/app/auth/callback/route.ts
+++ front/src/app/auth/callback/route.ts
@@ -2,6 +2,8 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
+const BASE_URL = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000'
+
 export async function GET(request: Request) {
     const requestUrl = new URL(request.url)
     const code = requestUrl.searchParams.get('code')
@@ -11,6 +13,5 @@ export async function GET(request: Request) {
         await supabase.auth.exchangeCodeForSession(code)
     }
 
-    // URL to redirect to after sign in process completes
-    return NextResponse.redirect(requestUrl.origin + '/dashboard')
+    return NextResponse.redirect( BASE_URL + '/dashboard')
 }
